5970331abcda76ff7fead2df17380ef0a97375cb
mehdi ben hassen <mehdi.ben.hassen@levio.ca>
2025-08-31 17:27:37 -0400

    update end point

--- sell_furniture/src/app/services/location.service.ts
+++ sell_furniture/src/app/services/location.service.ts
@@ -16,8 +16,8 @@ export interface items {
   providedIn: 'root',
 })
 export class LocationService {
-  private apiUrl = 'https://sell_furniture-node.onrender.com/api/locations'; // ✅ backend URL
-  private apiUrlItems = 'https://sell_furniture-node.onrender.com/api/items'; // ✅ backend URL
+  private apiUrl = 'https://sell-furniture-node.onrender.com/api/locations'; // ✅ backend URL
+  private apiUrlItems = 'https://sell-furniture-node.onrender.com/api/items'; // ✅ backend URL
 
   constructor(private http: HttpClient) {}
 
