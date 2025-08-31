081470a655ccb32a2607608bc760e93021f3abc8
mehdi ben hassen <mehdi.ben.hassen@levio.ca>
2025-08-31 17:15:02 -0400

    replace sell-furniture by sell_furniture2

---
+++ sell_furniture/src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { LocationService } from './services/location.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './components/main/main.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule, CommonModule, HttpClientModule, MainComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'], // ✅ fixed plural
+})
+export class AppComponent implements OnInit {
+  locations: any[] = [];
+  items: any[] = [];
+  newLocation = '';
+  title = 'sell_furniture';
+
+  constructor(private locationService: LocationService) {
+    this.loadLocations();
+    this.loadItems();
+  }
+
+  ngOnInit() {
+    this.loadLocations();
+    this.loadItems();
+  }
+
+  loadLocations() {
+    this.locationService.getLocations().subscribe((data) => {
+      this.locations = data;
+    });
+  }
+
+  loadItems() {
+    this.locationService.getItems().subscribe((data) => {
+      this.items = data;
+    });
+  }
+
+  addLocation() {
+    if (this.newLocation.trim()) {
+      this.locationService
+        .addLocation({ name: this.newLocation })
+        .subscribe((loc) => {
+          this.locations.push(loc);
+          this.newLocation = '';
+        });
+    }
+  }
+}
