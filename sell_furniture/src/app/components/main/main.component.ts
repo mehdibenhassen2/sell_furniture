600a8b7a7ceeb3b6d587c88ecec7a4b4bdf483b4
mehdi ben hassen <mehdi.ben.hassen@levio.ca>
2025-09-03 17:58:47 -0400

    add chairs and others

--- sell_furniture/src/app/components/main/main.component.ts
+++ sell_furniture/src/app/components/main/main.component.ts
@@ -24,7 +24,7 @@ export class MainComponent implements OnInit {
   items: any[] = [];
   newLocation = '';
   title = 'sell_furniture';
-  categories = ['Bed', 'Dresser', 'Technology', 'Decoration'];
+  categories = ['Bed', 'Dresser', 'Technology', 'Decoration', 'Chair', 'Other'];
   selectedCategories: string[] = [];
   filteredItems: any[] = [];
 
